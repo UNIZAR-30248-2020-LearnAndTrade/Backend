659c42df4432ec5bdb6a9c6c904ff2a2d2512817
Gonzalo Berne Melguizo <gera1397@gmail.com>
2020-10-23 11:52:10 +0200

    Creación proyecto Spring

---
+++ src/main/java/com/project/LearnAndTrade/LearnAndTradeApplication.java
@@ -0,0 +1,13 @@
+package com.project.LearnAndTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAndTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAndTradeApplication.class, args);
+	}
+
+}
