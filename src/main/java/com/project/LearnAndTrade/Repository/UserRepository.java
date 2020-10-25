8faedcc199bc3200a530a43da5c5440385d07d33
Gonzalo Berne Melguizo <gera1397@gmail.com>
2020-10-25 12:06:47 +0100

    Repositorio User creado

---
+++ src/main/java/com/project/LearnAndTrade/Repository/UserRepository.java
@@ -0,0 +1,19 @@
+/*
+  UserRepository.java 1.0 25/10/2020
+ */
+
+/*
+  This interface is the repository for User
+  used for keep the persistence for the User entity
+
+  @author Gonzalo Berné
+  @version 1.0, 25/10/2020
+ */
+
+package com.project.LearnAndTrade.Repository;
+
+import com.project.LearnAndTrade.Entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, String> {
+}
