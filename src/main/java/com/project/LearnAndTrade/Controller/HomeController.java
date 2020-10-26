5fd65005ef2293de9e48324f6027940bd54f575e
Jorge Turbica <723883@unizar.es>
2020-10-26 23:10:31 +0100

    Added HomeController

---
+++ src/main/java/com/project/LearnAndTrade/Controller/HomeController.java
@@ -0,0 +1,29 @@
+/*
+ * HomeController.java 1.0 26/10/2020
+ */
+
+/*
+  This controller acts for the client petitions about home page
+
+  @author Jorge Turbica
+  @version 1.0, 26/10/2020
+ */
+
+package com.project.LearnAndTrade.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET})
+@RequestMapping(path = "/")
+public class HomeController {
+
+    @GetMapping(path = "/")
+    public ResponseEntity<Object> home() {
+        return ResponseEntity.status(HttpStatus.OK).body("Bienvenido!! :)");
+    }
+
+}
