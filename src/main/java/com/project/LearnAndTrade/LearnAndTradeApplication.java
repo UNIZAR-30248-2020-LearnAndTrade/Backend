4c167cd697f02efa8de47d47b6885570774e7f21
Gonzalo Berne Melguizo <gera1397@gmail.com>
2020-11-03 00:52:03 +0100

    Configuracion de seguridad añadida

--- src/main/java/com/project/LearnAndTrade/LearnAndTradeApplication.java
+++ src/main/java/com/project/LearnAndTrade/LearnAndTradeApplication.java
@@ -1,7 +1,15 @@
 package com.project.LearnAndTrade;
 
+import com.project.LearnAndTrade.Entity.User;
+import com.project.LearnAndTrade.Service.GetUserData;
+import com.project.LearnAndTrade.Service.SearchComplementaryUsers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class LearnAndTradeApplication {
