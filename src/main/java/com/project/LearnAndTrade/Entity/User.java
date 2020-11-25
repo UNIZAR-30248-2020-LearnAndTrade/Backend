34d70c7edf9009303c841e8699fb6fba1115b0a7
Gonzalo Berné Melguizo <gera1397@gmail.com>
2020-11-25 11:50:34 +0100

    Relacion de user con themes cambiado

--- src/main/java/com/project/LearnAndTrade/Entity/User.java
+++ src/main/java/com/project/LearnAndTrade/Entity/User.java
@@ -18,10 +18,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
@@ -46,14 +43,12 @@ public class User {
     private String password;
 
     @NotNull
-    @ElementCollection
-    @LazyCollection(LazyCollectionOption.FALSE)
+    @OneToMany(cascade = CascadeType.ALL)
     @Getter
     private List<Theme> interests;
 
     @NotNull
-    @ElementCollection
-    @LazyCollection(LazyCollectionOption.FALSE)
+    @OneToMany(cascade = CascadeType.ALL)
     @Getter
     private List<Theme> knowledges;
 
